f7fa9af76a94bb6cdf739cdd678273040bd03b0d
Victor Panisson <victor_panisson.estudante.sesisenai.org.br>
2024-06-20 16:39:34 -0300

    exercicio_5

---
+++ lista_1/exercicio_05/index.js
@@ -0,0 +1,8 @@
+function mostrarElemento() {
+var elemento = document.getElementById('elementoParaMostrarOcultar');
+elemento.style.display = 'block';
+}
+function ocultarElemento() {
+    var elemento = document.getElementById('elementoParaMostrarOcultar');
+    elemento.style.display = 'none';
+}
